1a651403c7375425c31e337ca02c5bb3c4e003e1
hernoufM <56631369+hernoufM@users.noreply.github.com>
2021-06-08 16:17:59 +0000

    Deploying to gh-pages from  @ a11d02007aaffb61d67be345fcc2cee998452f2f 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (38d7e4d) (2021/04/26 14:50)',
+    VERSION: 'master (a11d020) (2021/06/08 16:17)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
