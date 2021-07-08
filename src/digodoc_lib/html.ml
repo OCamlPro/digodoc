ec6ffea1ad46fa33f3fed9e250d312bc08b0c376
zapashcanon <leo@ndrs.fr>
2021-07-08 15:58:11 +0200

    clean generated html

--- src/digodoc_lib/html.ml
+++ src/digodoc_lib/html.ml
@@ -17,24 +17,24 @@ let digodoc_html_dir = Globals.digodoc_dir // "html"
 (* generate page _digodoc/html/${filename} *)
 let generate_page ~filename ~title f =
 
-  let dirname = EzFile.dirname filename in 
-  let path_list = 
-      if String.contains filename '/'   
-      then 
-        String.split_on_char '/' dirname 
-      else [] in
+  let dirname = EzFile.dirname filename in
+  let path_list =
+    if String.contains filename '/'
+    then
+      String.split_on_char '/' dirname
+    else [] in
   let s =
     String.concat "/"
-      (List.map (fun _s -> "..") 
-        path_list) in
+      (List.map (fun _s -> "..")
+          path_list) in
   let root = if s = "" then s else s ^ "/" in
-  
+
   let script = if !Globals.dynamic_index then "search_api.js" else "search.js" in
 
   (* removed 'async' from the script line because unrecognized by ez_ml parser *)
   let bb = Buffer.create 10000 in
   Printf.bprintf bb {|<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="en">
  <head>
   <title>%s</title>
   <link rel="stylesheet" href="%s_odoc-theme/odoc.css"/>
@@ -72,27 +72,27 @@ let check_html ~file xml =
   let rec iter =  function
     | PCData _ -> ()
     | Element (tagName, attributes, childNodes) ->
-        List.iter iter childNodes;
-        match tagName with
-        | "a" | "A" ->
-            List.iter (function
-                  ("href", link) ->
-                    begin
-                      if link = "" then
-                        Printf.eprintf "Empty Link in file %S\n%!" file
-                      else
-                        let link, _anchor = EzString.cut_at link '#' in
-                        if link <> "" &&
-                           not (EzString.starts_with link ~prefix:"http") then
-                          let dir = Filename.dirname file in
-                          let linked_file = dir // link in
-                          if not (Sys.file_exists linked_file) then
-                            Printf.eprintf "File %s: link %s is dangling\n  %s does not exist\n%!"
-                              file link linked_file
+      List.iter iter childNodes;
+      match tagName with
+      | "a" | "A" ->
+        List.iter (function
+            ("href", link) ->
+            begin
+              if link = "" then
+                Printf.eprintf "Empty Link in file %S\n%!" file
+              else
+                let link, _anchor = EzString.cut_at link '#' in
+                if link <> "" &&
+                   not (EzString.starts_with link ~prefix:"http") then
+                  let dir = Filename.dirname file in
+                  let linked_file = dir // link in
+                  if not (Sys.file_exists linked_file) then
+                    Printf.eprintf "File %s: link %s is dangling\n  %s does not exist\n%!"
+                      file link linked_file
 
-                    end
-                | _ -> ()) attributes
-        | _ -> ()
+            end
+          | _ -> ()) attributes
+      | _ -> ()
   in
   iter xml
 
@@ -101,33 +101,33 @@ let rec add_trailer list =
   | Element ("div", [ "id", "trailer" ], _ ) :: _ -> list
   | e :: list -> e :: add_trailer list
   | [] ->
-      HTML.CONS.[
-        div ~a:[ "id", "trailer" ]
-          [ hr ;
-            p [ s "Generated using ";
-                a ~a:[
-                  "href", "https://github.com/OCamlPro/digodoc";
-                  "target", "digodoc";
-                ]
-                  [ s "digodoc" ];
-                s " at ";
-                a ~a:[
-                  "href", "https://www.ocamlpro.com";
-                  "target", "ocamlpro";
-                ]
-                  [ s "OCamlPro" ];
-              ]
+    HTML.CONS.[
+      div ~a:[ "id", "trailer" ]
+        [ hr ;
+          p [ s "Generated using ";
+            a ~a:[
+              "href", "https://github.com/OCamlPro/digodoc";
+              "target", "digodoc";
+            ]
+              [ s "digodoc" ];
+            s " at ";
+            a ~a:[
+              "href", "https://www.ocamlpro.com";
+              "target", "ocamlpro";
+            ]
+              [ s "OCamlPro" ];
           ]
-      ]
+        ]
+    ]
 
 let insert_trailer xml =
   let rec iter xml =
     match xml with
     | PCData _ -> xml
     | Element ("div", [ "class", "content" ], childNodes) ->
-        Element ("div", [ "class", "content" ], add_trailer childNodes)
+      Element ("div", [ "class", "content" ], add_trailer childNodes)
     | Element (tagName, attributes, childNodes) ->
-        Element ( tagName, attributes, List.map iter childNodes )
+      Element ( tagName, attributes, List.map iter childNodes )
   in
   iter xml
 
@@ -137,94 +137,94 @@ let iter_html ?(check_links=false) ?(add_trailer=false) dir =
   (*  EzFile.make_select *)
   EzFile.make_select EzFile.iter_dir ~deep:true ~glob:"*.html"
     ~f:(fun path ->
-        let file = dir // path in
-        match
-          match HTML.parse_file file with
-          | exception HTML.Error error ->
-              Printf.eprintf "%s: invalid html (%s)\n%!"
-                file (HTML.string_of_error error);
-              None
-          | xml -> Some xml
-        with
-        | None -> ()
-        | Some xml ->
-            if check_links then check_html ~file xml;
-            if add_trailer then
-              EzFile.write_file file
-                ( "<!DOCTYPE html>\n" ^ HTML.to_string ( insert_trailer xml ) )
-      ) dir;
+      let file = dir // path in
+      match
+        match HTML.parse_file file with
+        | exception HTML.Error error ->
+          Printf.eprintf "%s: invalid html (%s)\n%!"
+            file (HTML.string_of_error error);
+          None
+        | xml -> Some xml
+      with
+      | None -> ()
+      | Some xml ->
+        if check_links then check_html ~file xml;
+        if add_trailer then
+          EzFile.write_file file
+            ( "<!DOCTYPE html>\n" ^ HTML.to_string ( insert_trailer xml ) )
+    ) dir;
   Printf.eprintf "Scan finished.\n%!"
 
 let file_content filename =
   match Sys.getenv "DIGODOC_CONFIG" with
-  | dir when EzFile.exists (dir // filename) -> 
+  | dir when EzFile.exists (dir // filename) ->
     EzFile.read_file (dir // filename)
   | exception Not_found | _ ->
-    begin   
+    begin
       match Htmlize.Files.read filename with
       | None -> ""
       | Some file_content -> file_content
     end
 
 let add_header_footer () =
-  let open Htmlize in 
+  let open Htmlize in
   Printf.eprintf "Adding header and footer...\n%!";
   let html_dir = digodoc_html_dir in
   let head_childs = [("link", {|<link rel="icon" href="${root-html}_odoc-theme/favicon.png">|});
-                     ("script", {|<script defer="defer" 
-                        type="application/javascript" 
+    ("script", {|<script defer="defer"
+                        type="application/javascript"
                         src="${root-html}headerFooter.js">
                         </script>|})] in
-  
+
 
   EzFile.make_select EzFile.iter_dir ~deep:true ~glob:"*.html"
     ~f:(fun path ->
-        if EzString.starts_with ~prefix:"ENTRY" (EzFile.basename path) 
-        then ()
-        else begin
-          let file = html_dir // path in
-          let rec brace () var = 
-            match var with
-            | "root-html" ->
-              let dirname = EzFile.dirname path in 
-              let path_list = 
-                if String.contains path '/'   
-                then 
-                  String.split_on_char '/' dirname 
-                else []
-              in
-              let s =
-                String.concat "/"
-                  (List.map (fun _s -> "..") 
+      if EzString.starts_with ~prefix:"ENTRY" (EzFile.basename path)
+      then ()
+      else begin
+        let file = html_dir // path in
+        let rec brace () var =
+          match var with
+          | "root-html" ->
+            let dirname = EzFile.dirname path in
+            let path_list =
+              if String.contains path '/'
+              then
+                String.split_on_char '/' dirname
+              else []
+            in
+            let s =
+              String.concat "/"
+                (List.map (fun _s -> "..")
                     path_list)
-              in
-              if s = "" then s else s ^ "/"
-            | "sources" -> 
-              if !Globals.sources 
-              then 
-                Printf.sprintf {|<a id="sources-item" href="%ssources.html">Sources</a>|}
+            in
+            if s = "" then s else s ^ "/"
+          | "sources" ->
+            if !Globals.sources
+            then
+              Printf.sprintf {|<a id="sources-item" href="%ssources.html">Sources</a>|}
                 (brace () "root-html")
-              else ""
-            | "header_link" ->
-              if !Globals.with_header 
-              then {| | <a href="#header">To the top</a>|} 
-              else ""
-            | _ -> 
-              Printf.kprintf failwith "Unknown var %S" var
-          in
-        
-          let html = EzFile.read_file file 
-          and header = Ez_subst.V1.EZ_SUBST.string (file_content "header.html") ~brace ~ctxt:()
-          and footer = Ez_subst.V1.EZ_SUBST.string (file_content "footer.html") ~brace ~ctxt:()
-          and head_childs = List.map (fun (id,child) -> id, Ez_subst.V1.EZ_SUBST.string child ~brace ~ctxt:()) head_childs in
+            else ""
+          | "header_link" ->
+            if !Globals.with_header
+            then {| | <a href="#header">To the top</a>|}
+            else ""
+          | _ ->
+            Printf.kprintf failwith "Unknown var %S" var
+        in
 
-          let html' = Patchtml.edit_html ~header ~footer ~head_childs html in
+        let html = EzFile.read_file file
+        and header = Ez_subst.V1.EZ_SUBST.string (file_content "header.html") ~brace ~ctxt:()
+        and footer = Ez_subst.V1.EZ_SUBST.string (file_content "footer.html") ~brace ~ctxt:()
+        and head_childs = List.map (fun (id,child) -> id, Ez_subst.V1.EZ_SUBST.string child ~brace ~ctxt:()) head_childs in
 
-          EzFile.remove file;
+        let html' = Patchtml.edit_html ~header ~footer ~head_childs html in
 
-          EzFile.write_file file html'
-        end
-      ) html_dir
+        EzFile.remove file;
+
+        EzFile.write_file file html'
+      end
+    ) html_dir
 
 
 let write_file file ~content =
